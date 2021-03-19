acb6e5ee3bfe55b1b5377fabe7c1accd19c13059
olivier <olivier@kreako.fr>
2021-03-19 22:23:30 +0100

    Add home vue page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from './components/Login.vue'
 import Signup from './components/Signup.vue'
 import LostPassword from './components/LostPassword.vue'
+import Home from './pages/Home.vue'
 
 const routes = [
-    { path: '/', redirect: '/login' },
+    { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '/lost-password', component: LostPassword },
     { path: '/signup', component: Signup }
