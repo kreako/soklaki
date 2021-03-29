47166555b065b1ef476af28ae495df8591269ec2
olivier <olivier@kreako.fr>
2021-03-29 20:50:19 +0200

    Students and Socle store model

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,7 +16,20 @@ const state = {
         name: null,
         email: null,
         emailConfirmed: null,
-    }
+    },
+    students: [],
+    socle: {
+        // cache id -> object
+        domains: {},
+        components: {},
+        competencies: {},
+        subjects: {},
+        // Contains only id
+        c1: [],
+        c2: [],
+        c3: [],
+        c4: [],
+    },
 }
 
 const mutations = {
@@ -67,6 +80,36 @@ const mutations = {
         }
         state.login.groupId = groupId
     },
+    setStudents(state, students) {
+        state.students = students
+    },
+    setSocle(state, socle) {
+        state.socle.c1 = socle.c1
+        state.socle.c2 = socle.c2
+        state.socle.c3 = socle.c3
+        state.socle.c4 = socle.c4
+        // Now update cache
+        state.socle.domains = {}
+        for (const idx in socle.domains) {
+            const domain = socle.domains[idx]
+            state.socle.domains[domain.id] = domain
+        }
+        state.socle.components = {}
+        for (const idx in socle.components) {
+            const component = socle.components[idx]
+            state.socle.components[component.id] = component
+        }
+        state.socle.competencies = {}
+        for (const idx in socle.competencies) {
+            const competency = socle.competencies[idx]
+            state.socle.competencies[competency.id] = competency
+        }
+        state.socle.subjects = {}
+        for (const idx in socle.subjects) {
+            const subject = socle.subjects[idx]
+            state.socle.subjects[subject.id] = subject
+        }
+    },
 }
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
@@ -93,8 +136,19 @@ const actions = {
     },
 
     async resetPassword({ commit }, { email }) {
+        // TODO
         window.console.log('dev', import.meta.env.DEV)
         window.console.log('url', import.meta.env.VITE_API_URL)
+    },
+
+    async students({ commit }) {
+        let answer = await axios.get('students')
+        commit('setStudents', answer.data.student)
+    },
+
+    async socle({ commit }) {
+        let answer = await axios.get('socle')
+        commit('setSocle', answer.data)
     },
 }
 
