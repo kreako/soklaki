364f170257b2be16c200219af97e8ca5c2289ebd
olivier <olivier@kreako.fr>
2021-04-06 02:00:49 +0200

    Add a way to cancel student selection

--- frontend/src/components/StudentSelector.vue
+++ frontend/src/components/StudentSelector.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <input
-      v-model="filter"
-      class="input w-full mt-2"
-      type="text"
-      placeholder="Filtre..."
-    />
+    <div class="flex flex-row items-center mt-2">
+      <input
+        v-model="filter"
+        class="input w-full flex-grow"
+        type="text"
+        placeholder="Filtre..."
+      />
+      <button alt="Abandonner la sélection" @click="$emit('cancel')">
+        <IconXCircle class="text-gray-300 h-4 mx-2 hover:text-gray-600" />
+      </button>
+    </div>
     <div class="grid grid-cols-1 max-h-46 overflow-y-auto mt-2 bg-gray-50">
       <div
         v-for="id in filteredStudents"
         :key="id"
         class="px-2 flex flex-row items-center gap-2"
       >
-        <button class="flex flex-row" @click="$emit('select', id)">
-          <IconPlusCircle class="w-4 text-gray-300 mr-2" />
-          {{ students[id].firstname }} {{ students[id].lastname }}
+        <button class="group flex flex-row" @click="$emit('select', id)">
+          <IconPlusCircle
+            class="w-4 text-gray-300 group-hover:text-gray-600 mr-2"
+          />
+          <div>{{ students[id].firstname }} {{ students[id].lastname }}</div>
+        </button>
+      </div>
+      <div class="px-2 flex flex-row items-center gap-2">
+        <button class="group flex flex-row" @click="$emit('cancel')">
+          <IconXCircle class="w-4 text-red-300 group-hover:text-red-600 mr-2" />
+          <div>Ne sélectionnez... personne !</div>
         </button>
       </div>
     </div>
@@ -25,8 +38,9 @@
 import { defineEmit, defineProps, ref, computed } from "vue";
 import IconX from "../icons/IconX.vue";
 import IconPlusCircle from "../icons/IconPlusCircle.vue";
+import IconXCircle from "../icons/IconXCircle.vue";
 
-defineEmit(["select"]);
+defineEmit(["select", "cancel"]);
 
 const props = defineProps({
   students: Object,
