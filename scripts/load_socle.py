b9d45f42658122fc9add734355dbbb93567fc39e
olivier <olivier@kreako.fr>
2021-04-08 23:08:30 +0200

    load socle script

--- scripts/load_socle.py
+++ scripts/load_socle.py
@@ -1,43 +1,73 @@
 import logging
 import csv
-from models import SocleDomain, SocleSubject, SocleComponent, SocleCompetency, SocleCompetencySubject, database
+from models import (
+    SocleContainer,
+    SocleSubject,
+    SocleCompetency,
+    SocleCompetencySubject,
+    database,
+)
 
 
-logger = logging.getLogger('root')
+logger = logging.getLogger("root")
 FORMAT = "[%(lineno)s:%(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
 
 
-def parse_domain(line):
-    rank = int(line[0])
-    title = line[1]
-    if line[2] != "" or line[3] != "":
-        logger.debug(str(line))
-        raise ValueError(line)
-    domain = SocleDomain(rank=rank, title=title)
-    domain.save()
-    return domain
+def parse_container(cycle, container, line):
+    if line[0] != "" and line[1] != "" and line[2] == "" and line[3] == "":
+        # level 1 container
+        rank = int(line[0])
+        text = line[1]
+        c = SocleContainer(
+            container=None,
+            cycle=cycle,
+            rank=rank,
+            full_rank=f"{rank}.",
+            alpha_full_rank=f"{rank:02d}.",
+            text=text,
+        )
+        c.save()
+        return c
+    if line[0] == "" and line[1] != "" and line[2] != "" and line[3] == "":
+        # level 2 container
+        rank = int(line[1])
+        text = line[2]
+        if container.container is not None:
+            # root container is not yet a level 1
+            container = container.container
+        full_rank = f"{container.rank}.{rank}."
+        alpha_full_rank = f"{container.rank:02d}.{rank:02d}."
+        c = SocleContainer(
+            container=container,
+            cycle=cycle,
+            rank=rank,
+            full_rank=full_rank,
+            alpha_full_rank=alpha_full_rank,
+            text=text,
+        )
+        c.save()
+        return c
+    raise ValueError(line)
 
 
-def parse_component(domain, line):
-    rank = int(line[1])
-    title = line[2]
-    if line[0] != "" or line[3] != "":
-        logger.debug(str(line))
-        raise ValueError(line)
-    component = SocleComponent(domain=domain, rank=rank, title=title)
-    component.save()
-    return component
-
-
-def parse_competency(cycle, component, line):
+def parse_competency(cycle, container, line):
     rank = int(line[2])
     text = line[3]
     if line[0] != "" or line[1] != "":
         logger.debug(str(line))
         raise ValueError(line)
-    competency = SocleCompetency(component=component, rank=rank, text=text, cycle=cycle)
+    full_rank = f"{container.full_rank}{rank}."
+    alpha_full_rank = f"{container.alpha_full_rank}{rank:02d}."
+    competency = SocleCompetency(
+        container=container,
+        full_rank=full_rank,
+        alpha_full_rank=alpha_full_rank,
+        rank=rank,
+        text=text,
+        cycle=cycle,
+    )
     competency.save()
     return competency
 
@@ -52,25 +82,23 @@ def parse_subjects(competency, line):
 def main(cycle, input_filename):
     with open(input_filename, "r") as f:
         reader = csv.reader(f)
-        domain = None
-        component = None
-        while True:
-            try:
+        container = None
+        try:
+            while True:
                 line = next(reader)
-            except StopIteration:
-                break
-            try:
-                domain = parse_domain(line)
-            except ValueError:
+                print(line)
                 try:
-                    component = parse_component(domain, line)
+                    container = parse_container(cycle, container, line)
                 except ValueError:
                     # on 2 lines
-                    competency = parse_competency(cycle, component, line)
+                    competency = parse_competency(cycle, container, line)
                     line = next(reader)
                     parse_subjects(competency, line)
+        except StopIteration:
+            return
 
 
 if __name__ == "__main__":
-    import sys
-    main(sys.argv[1], sys.argv[2])
+    main("c1", "cycle_1.csv")
+    main("c2", "cycle_2.csv")
+    main("c3", "cycle_3.csv")
