6e342f5656d7f683cda7e6b8ef3eeb1eb41db177
olivier <olivier@kreako.fr>
2021-04-13 21:04:56 +0200

    Throw user facing errors if date string is not in format

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -10,12 +10,44 @@ export const dateFromString = (dt) => {
 
 /// Build a date javascript object from a date string
 /// like 2021-03-30T17:59:16.637699+00:00 or 2021-03-30
+/// Throws errors (trying to be explicit if something is wrong)
 export const dateJsObj = (dt) => {
-  // Not using Date.parse because as MDN states : Parsing of date strings
-  // is strongly discouraged due to browser differences and inconsistencies.
-  const year = dt.slice(0, 4);
-  const month = dt.slice(5, 7);
-  const day = dt.slice(8, 10);
+  const year = Number(dt.slice(0, 4));
+  const separator1 = dt[4];
+  const month = Number(dt.slice(5, 7));
+  const separator2 = dt[7];
+  const day = Number(dt.slice(8, 10));
+  if (separator1 !== "-") {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Chaque élément séparé par un tiret '-'.\n
+    Ici, le premier séparateur n'est pas un tiret '-', mais ${separator1} !`);
+  }
+  if (separator2 !== "-") {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Chaque élément séparé par un tiret '-'.\n
+    Ici, le deuxième séparateur n'est pas un tiret '-', mais ${separator2} !`);
+  }
+  if (!Number.isInteger(year) || year > 2100 || year < 1900) {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Ici, l'année '${dt.slice(0, 4)}' ne me semble pas valide !`);
+  }
+  if (!Number.isInteger(month) || month > 12 || month < 1) {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Ici, le mois '${dt.slice(5, 7)}' ne me semble pas valide !`);
+  }
+  if (!Number.isInteger(day) || day > 31 || day < 1) {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Ici, le jour '${dt.slice(8, 10)}' ne me semble pas valide !`);
+  }
+  const date = new Date(year, month - 1, day);
+  if (
+    year !== date.getFullYear() ||
+    month !== date.getMonth() + 1 ||
+    day !== date.getDate()
+  ) {
+    throw new Error(`Le format de la date est <année:4 chiffres>-<mois:2 chiffres>-<jour:2 chiffres>.\n
+    Ici, la date ne me semble pas valide ?`);
+  }
   return new Date(year, month, day);
 };
 
