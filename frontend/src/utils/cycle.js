e089945e265915c7d3c7a9ca6cd612440603bb57
olivier <olivier@kreako.fr>
2021-04-19 10:40:17 +0200

    Cycle full name from abbreviation

--- frontend/src/utils/cycle.js
+++ frontend/src/utils/cycle.js
@@ -2,6 +2,20 @@ import { dateJsObj, dateDiffInDays } from "./date";
 
 export const cycleNb = (cycle) => cycle[1];
 
+export const cycleFullName = (cycle) => {
+  if (cycle === "c1") {
+    return "Cycle 1";
+  } else if (cycle == "c2") {
+    return "Cycle 2";
+  } else if (cycle == "c3") {
+    return "Cycle 3";
+  } else if (cycle == "c4") {
+    return "Cycle 4";
+  } else {
+    return null;
+  }
+};
+
 // TODO : trying to compensate for bisextile year
 // But maybe 1 day, it will fail miserably
 const YEARS_6 = 6 * 365 + 1;
