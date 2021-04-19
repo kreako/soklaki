e710a83257077d57390c1d19fd8218ef069ff371
olivier <olivier@kreako.fr>
2021-04-19 12:24:38 +0200

    List observations

--- frontend/src/pages/Observations.vue
+++ frontend/src/pages/Observations.vue
@@ -2,9 +2,35 @@
   <div class="mt-4 px-2">
     <div>Observations</div>
     <div>
-      <div v-for="observationId in sortedObservations">
-        {{ observations[observationId].date }}
-        {{ observations[observationId].text }}
+      <div v-for="observationId in sortedObservations" class="mt-6">
+        <div class="flex flex-row items-center space-x-4">
+          <div>
+            <router-link :to="`/observation/${observationId}`">
+              {{ observations[observationId].date }}
+            </router-link>
+          </div>
+          <div class="text-xs rounded-full px-1 border border-gray-600">
+            {{ userInitials(userById(observations[observationId].user.id)) }}
+          </div>
+          <div class="flex-grow"></div>
+          <div class="flex flex-row items-center space-x-2">
+            <div>
+              {{ observations[observationId].students.length }}
+            </div>
+            <IconUser class="h-4 text-gray-500" />
+          </div>
+          <div class="flex flex-row items-center space-x-2">
+            <div>
+              {{ observations[observationId].competencies.length }}
+            </div>
+            <IconCompetency class="h-4 text-gray-500" />
+          </div>
+        </div>
+        <div class="truncate">
+          <router-link :to="`/observation/${observationId}`">
+            {{ observations[observationId].text }}
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -12,8 +38,13 @@
 <script setup>
 import { useStore } from "vuex";
 import { computed, ref, onMounted, watch } from "vue";
+import { userInitials } from "../utils/user";
+import IconUser from "../icons/IconUser.vue";
+import IconCompetency from "../icons/IconCompetency.vue";
 
 const store = useStore();
+
+const userById = computed(() => store.getters.userById);
 
 const observations = computed(() => store.state.observations);
 const sortedObservations = computed(
