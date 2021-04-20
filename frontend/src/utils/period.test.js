231dd7e65d9ed4b7139a1f8477b9ce1555119b0e
olivier <olivier@kreako.fr>
2021-04-21 00:06:06 +0200

    Add period unit testing

---
+++ frontend/src/utils/period.test.js
@@ -0,0 +1,33 @@
+import { searchPeriod } from "./period";
+
+test("searchPeriod", () => {
+  const periods = {
+    1: {
+      id: 1,
+      start: "2019-08-29",
+      end: "2020-07-11",
+    },
+    2: {
+      id: 2,
+      start: "2020-08-29",
+      end: "2021-07-11",
+    },
+  };
+  // inside
+  let period = searchPeriod("2020-09-18", periods);
+  expect(period.id).toBe(2);
+  // end frontier
+  period = searchPeriod("2020-07-11", periods);
+  expect(period.id).toBe(1);
+  // start frontier
+  period = searchPeriod("2020-08-29", periods);
+
+  // Not found cases
+  expect(period.id).toBe(2);
+  // in between
+  expect(searchPeriod("2020-07-12", periods)).toBeNull();
+  // before
+  expect(searchPeriod("2019-08-28", periods)).toBeNull();
+  // after
+  expect(searchPeriod("2021-07-12", periods)).toBeNull();
+});
