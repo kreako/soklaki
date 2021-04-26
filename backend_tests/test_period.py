c4cdcf508a9c9b91117b265bf798c33a698134b5
olivier <olivier@kreako.fr>
2021-04-26 15:17:41 +0200

    Add insert period test

---
+++ backend_tests/test_period.py
@@ -0,0 +1,41 @@
+from . import client
+
+# TODO
+# insert-period
+# periods
+
+
+def test_insert_period(login):
+    code, data = client.post("periods", {}, login["token"])
+    assert code == 200
+    before_periods = data["periods"]
+
+    code, data = client.post(
+        "insert-period",
+        {
+            "group_id": login["group_id"],
+            "name": "ma période",
+            "start": "2014-06-01",
+            "end": "2015-09-01",
+        },
+        login["token"],
+    )
+    assert code == 200
+    id = data["insert_eval_period_one"]["id"]
+
+    code, data = client.post("periods", {}, login["token"])
+    assert code == 200
+    after_periods = data["periods"]
+
+    assert len(before_periods) + 1 == len(after_periods)
+
+    for period in after_periods:
+        if period["id"] == id:
+            assert period["name"] == "ma période"
+            assert period["start"] == "2014-06-01"
+            assert period["end"] == "2015-09-01"
+            break
+    else:
+        print(after_periods)
+        # If I'm here, it means that my new period is not in periods
+        assert 1 == 0
