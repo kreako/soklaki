e7cdde7ba41360cbd1ad8fdc34b4d08d056327af
olivier <olivier@kreako.fr>
2021-05-03 01:37:32 +0200

    Refactor competency selector to use new utilities

--- frontend/src/components/CompetencySelector.vue
+++ frontend/src/components/CompetencySelector.vue
@@ -145,6 +145,14 @@
 <script setup>
 import { defineProps, defineEmit, ref, computed } from "vue";
 import { cycleNb } from "../utils/cycle";
+import {
+  filterSocleBySubject,
+  filterSocleBy2Ranks,
+  filterSocleBy3Ranks,
+  filterSocleByRegex,
+  filterSocleByContainerId,
+  filterSocleByContainerIds,
+} from "../utils/socle";
 import IconChevronDown from "../icons/IconChevronDown.vue";
 import IconChevronRight from "../icons/IconChevronRight.vue";
 import IconBackSpace from "../icons/IconBackSpace.vue";
@@ -158,14 +166,6 @@ const props = defineProps({
 const cycleAsNb = computed(() => cycleNb(props.cycle));
 
 const selectedSubject = ref(-1);
-const isInSubjects = (competency, subjectId) => {
-  for (let i = 0; i < competency.subjects.length; i++) {
-    if (competency.subjects[i].subject.id === subjectId) {
-      return true;
-    }
-  }
-  return false;
-};
 
 const selectedContainer1 = ref(null);
 const selectContainer1 = (id) => {
@@ -201,136 +201,60 @@ const selectCompetency = (competencyId) => {
 
 const socleFilter = ref("");
 
-const searchObjectIdInArray = (array, o) => {
-  for (const e of array) {
-    if (e.id === o.id) {
-      return e;
-    }
-  }
-  array.push(o);
-  return array[array.length - 1];
-};
-
 const cheatcodeRe = /(\d{1,2})[\. ](\d{1,2})[\. ]?(\d{1,2})?/;
 
 const filteredSocle = computed(() => {
+  const socle = props.socle[props.cycle];
   const cheatSocleAccess = socleFilter.value.match(cheatcodeRe);
   if (cheatSocleAccess != null) {
     // OMG an advanced user typing the direct access to the competency
     if (cheatSocleAccess[3] == null) {
       const container1Rank = cheatSocleAccess[1];
       const competencyRank = cheatSocleAccess[2];
-      // Let's find it
-      for (const container1 of props.socle[props.cycle]) {
-        if (props.socle.containers[container1.id].rank != container1Rank) {
-          continue;
-        }
-        for (const competency of container1.competencies) {
-          if (props.socle.competencies[competency.id].rank == competencyRank) {
-            return [
-              {
-                id: container1.id,
-                competencies: [competency],
-              },
-            ];
-          }
-        }
+      const f = filterSocleBy2Ranks(
+        props,
+        socle,
+        container1Rank,
+        competencyRank
+      );
+      if (f.length > 0) {
+        return f;
       }
     } else {
       const container1Rank = cheatSocleAccess[1];
       const container2Rank = cheatSocleAccess[2];
       const competencyRank = cheatSocleAccess[3];
-      // Let's find it
-      for (const container1 of props.socle[props.cycle]) {
-        if (props.socle.containers[container1.id].rank != container1Rank) {
-          continue;
-        }
-        for (const container2 of container1.children) {
-          if (props.socle.containers[container2.id].rank != container2Rank) {
-            continue;
-          }
-          for (const competency of container2.competencies) {
-            if (
-              props.socle.competencies[competency.id].rank == competencyRank
-            ) {
-              return [
-                {
-                  id: container1.id,
-                  children: [
-                    {
-                      id: container2.id,
-                      competencies: [competency],
-                    },
-                  ],
-                },
-              ];
-            }
-          }
-        }
+      const f = filterSocleBy3Ranks(
+        props,
+        socle,
+        container1Rank,
+        container2Rank,
+        competencyRank
+      );
+      if (f.length > 0) {
+        return f;
       }
     }
   }
   // Let's build a socle tree with competencies matching the filter
-  // of this non advanced user, expecting text filter
-  let r = new RegExp(socleFilter.value, "i");
-  const socle = [];
-  for (const container1 of props.socle[props.cycle]) {
-    if (
-      selectedContainer1.value != null &&
-      container1.id !== selectedContainer1.value
-    ) {
-      // Ignore non selected container1
-      continue;
-    }
-    for (const container2 of container1.children) {
-      if (
-        selectedContainer2.value != null &&
-        container2.id !== selectedContainer2.value
-      ) {
-        // Ignore non selected container2
-        continue;
-      }
-      for (const competency of container2.competencies) {
-        if (selectedSubject.value !== -1) {
-          if (!isInSubjects(competency, selectedSubject.value)) {
-            // Do not select competencies not in selected subject
-            continue;
-          }
-        }
-        if (r.test(props.socle.competencies[competency.id].text)) {
-          // This is a match so add this competency with the corresponding container1/container2
-          const c1 = searchObjectIdInArray(socle, {
-            id: container1.id,
-            children: [],
-            competencies: [],
-          });
-          const c2 = searchObjectIdInArray(c1.children, {
-            id: container2.id,
-            competencies: [],
-          });
-          c2.competencies.push(competency);
-        }
-      }
-    }
-    for (const competency of container1.competencies) {
-      if (selectedSubject.value !== -1) {
-        if (!isInSubjects(competency, selectedSubject.value)) {
-          // Do not select competencies not in selected subject
-          continue;
-        }
-      }
-      if (r.test(props.socle.competencies[competency.id].text)) {
-        // This is a match so add this competency with the corresponding container1/container2
-        const c1 = searchObjectIdInArray(socle, {
-          id: container1.id,
-          children: [],
-          competencies: [],
-        });
-        c1.competencies.push(competency);
-      }
+  // of this non advanced user, expecting subject or text filter
+  let f = socle;
+  if (selectedContainer1.value != null) {
+    if (selectedContainer2.value != null) {
+      f = filterSocleByContainerIds(
+        f,
+        selectedContainer1.value,
+        selectedContainer2.value
+      );
+    } else {
+      f = filterSocleByContainerId(f, selectedContainer1.value);
     }
   }
-  return socle;
+  if (selectedSubject.value !== -1) {
+    f = filterSocleBySubject(f, selectedSubject.value);
+  }
+  let r = new RegExp(socleFilter.value, "i");
+  return filterSocleByRegex(props, f, r);
 });
 
 const filtered = computed(() => {
