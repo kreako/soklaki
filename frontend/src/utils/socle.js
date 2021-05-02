74deeac8644cd7a3cac4e4ae7357ceec502dddbb
olivier <olivier@kreako.fr>
2021-05-03 01:37:08 +0200

    Utilities with tests to filter the socle (by ids, rank, subject or regex)

--- frontend/src/utils/socle.js
+++ frontend/src/utils/socle.js
@@ -1,3 +1,5 @@
+import { isSubjectInCompetency } from "./competency";
+
 const parentRanks = (state, containerId) => {
   if (containerId == null) {
     // Init case
@@ -15,4 +17,190 @@ export const computeRanks = ({ state, rank, containerId }) => {
   const alphaFullRank =
     parents.map((x) => x.toString().padStart(4, "0")).join(".") + ".";
   return { alphaFullRank, fullRank };
+};
+
+const filterCompetenciesBySubject = (competencies, subjectId) =>
+  competencies.filter((c) => isSubjectInCompetency(subjectId, c));
+
+const filterContainer2BySubject = (children, subjectId) => {
+  const filtered = [];
+  for (const container of children) {
+    const competencies = filterCompetenciesBySubject(
+      container.competencies,
+      subjectId
+    );
+    if (competencies.length > 0) {
+      filtered.push({
+        id: container.id,
+        competencies: competencies,
+      });
+    }
+  }
+  return filtered;
+};
+
+export const filterSocleBySubject = (socle, subjectId) => {
+  // assume that in container level 1 children and competencies are mutually exclusive
+  const filtered = [];
+  for (const container1 of socle) {
+    const competencies = filterCompetenciesBySubject(
+      container1.competencies,
+      subjectId
+    );
+    if (competencies.length > 0) {
+      filtered.push({
+        id: container1.id,
+        children: [],
+        competencies: competencies,
+      });
+      continue;
+    }
+    const children = filterContainer2BySubject(container1.children, subjectId);
+    if (children.length > 0) {
+      filtered.push({
+        id: container1.id,
+        children: children,
+        competencies: [],
+      });
+      continue;
+    }
+  }
+  return filtered;
+};
+
+export const filterSocleBy2Ranks = (state, socle, rank1, rank2) => {
+  for (const container1 of socle) {
+    if (state.socle.containers[container1.id].rank != rank1) {
+      continue;
+    }
+    for (const competency of container1.competencies) {
+      if (state.socle.competencies[competency.id].rank == rank2) {
+        return [
+          {
+            id: container1.id,
+            children: [],
+            competencies: [competency],
+          },
+        ];
+      }
+    }
+  }
+  return [];
+};
+
+export const filterSocleBy3Ranks = (state, socle, rank1, rank2, rank3) => {
+  for (const container1 of socle) {
+    if (state.socle.containers[container1.id].rank != rank1) {
+      continue;
+    }
+    for (const container2 of container1.children) {
+      if (state.socle.containers[container2.id].rank != rank2) {
+        continue;
+      }
+      for (const competency of container2.competencies) {
+        if (state.socle.competencies[competency.id].rank == rank3) {
+          return [
+            {
+              id: container1.id,
+              children: [
+                {
+                  id: container2.id,
+                  competencies: [competency],
+                },
+              ],
+              competencies: [],
+            },
+          ];
+        }
+      }
+    }
+  }
+  return [];
+};
+
+const isRegexCompetency = (state, competency, regex) =>
+  regex.test(state.socle.competencies[competency.id].text);
+
+const filterCompetenciesByRegex = (state, competencies, regex) =>
+  competencies.filter((c) => isRegexCompetency(state, c, regex));
+
+const filterContainer2ByRegex = (state, children, regex) => {
+  const filtered = [];
+  for (const container of children) {
+    const competencies = filterCompetenciesByRegex(
+      state,
+      container.competencies,
+      regex
+    );
+    if (competencies.length > 0) {
+      filtered.push({
+        id: container.id,
+        competencies: competencies,
+      });
+    }
+  }
+  return filtered;
+};
+
+export const filterSocleByRegex = (state, socle, regex) => {
+  // assume that in container level 1 children and competencies are mutually exclusive
+  const filtered = [];
+  for (const container1 of socle) {
+    const competencies = filterCompetenciesByRegex(
+      state,
+      container1.competencies,
+      regex
+    );
+    if (competencies.length > 0) {
+      filtered.push({
+        id: container1.id,
+        children: [],
+        competencies: competencies,
+      });
+      continue;
+    }
+    const children = filterContainer2ByRegex(state, container1.children, regex);
+    if (children.length > 0) {
+      filtered.push({
+        id: container1.id,
+        children: children,
+        competencies: [],
+      });
+      continue;
+    }
+  }
+  return filtered;
+};
+
+export const filterSocleByContainerId = (socle, container1Id) => {
+  for (const container1 of socle) {
+    if (container1.id === container1Id) {
+      return [container1];
+    }
+  }
+  return [];
+};
+
+export const filterSocleByContainerIds = (
+  socle,
+  container1Id,
+  container2Id
+) => {
+  for (const container1 of socle) {
+    if (container1.id !== container1Id) {
+      continue;
+    }
+    for (const container2 of container1.children) {
+      if (container2.id === container2Id) {
+        return [
+          {
+            id: container1.id,
+            children: [container2],
+            competencies: [],
+          },
+        ];
+      }
+    }
+  }
+  return [];
 };
--- frontend/src/utils/socle.test.js
+++ frontend/src/utils/socle.test.js
@@ -1,4 +1,12 @@
-import { computeRanks } from "./socle";
+import {
+  computeRanks,
+  filterSocleBySubject,
+  filterSocleBy2Ranks,
+  filterSocleBy3Ranks,
+  filterSocleByRegex,
+  filterSocleByContainerId,
+  filterSocleByContainerIds,
+} from "./socle";
 
 test("computeRanks with no parent", () => {
   const state = {};
@@ -24,4 +32,373 @@ test("computeRanks with 2 parent", () => {
   expect(
     computeRanks({ state: state, rank: 14, containerId: 23 })
   ).toStrictEqual({ alphaFullRank: "0202.0003.0014.", fullRank: "202.3.14." });
+});
+
+test("filterSocleBySubject", () => {
+  const socle = [
+    {
+      id: 23,
+      children: [
+        {
+          id: 23,
+          competencies: [{ id: 5, subjects: [{ id: 3, subject_id: 12 }] }],
+        },
+        {
+          id: 12,
+          competencies: [
+            {
+              id: 13,
+              subjects: [
+                {
+                  id: 4,
+                  subject_id: 14,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 24,
+          competencies: [
+            {
+              id: 5,
+              subjects: [
+                { id: 3, subject_id: 12 },
+                { id: 5, subject_id: 13 },
+              ],
+            },
+            {
+              id: 6,
+              subjects: [
+                { id: 3, subject_id: 12 },
+                { id: 5, subject_id: 14 },
+              ],
+            },
+            {
+              id: 5,
+              subjects: [
+                { id: 3, subject_id: 12 },
+                { id: 5, subject_id: 13 },
+              ],
+            },
+          ],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 24,
+      children: [],
+      competencies: [],
+    },
+    {
+      id: 25,
+      children: [],
+      competencies: [
+        {
+          id: 5,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 13 },
+          ],
+        },
+        {
+          id: 6,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 14 },
+          ],
+        },
+        {
+          id: 5,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 13 },
+          ],
+        },
+        {
+          id: 6,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 14 },
+          ],
+        },
+      ],
+    },
+  ];
+  expect(filterSocleBySubject(socle, 14)).toStrictEqual([
+    {
+      id: 23,
+      children: [
+        {
+          id: 12,
+          competencies: [{ id: 13, subjects: [{ id: 4, subject_id: 14 }] }],
+        },
+        {
+          id: 24,
+          competencies: [
+            {
+              id: 6,
+              subjects: [
+                { id: 3, subject_id: 12 },
+                { id: 5, subject_id: 14 },
+              ],
+            },
+          ],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 25,
+      children: [],
+      competencies: [
+        {
+          id: 6,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 14 },
+          ],
+        },
+        {
+          id: 6,
+          subjects: [
+            { id: 3, subject_id: 12 },
+            { id: 5, subject_id: 14 },
+          ],
+        },
+      ],
+    },
+  ]);
+});
+
+test("filterSocleBy2Ranks", () => {
+  const state = {
+    socle: {
+      containers: { 25: { rank: 19 }, 4: { rank: 23 } },
+      competencies: { 5: { rank: 19 }, 6: { rank: 23 }, 3: { rank: 19 } },
+    },
+  };
+  const socle = [
+    {
+      id: 25,
+      children: [],
+      competencies: [
+        { id: 5, subjects: [] },
+        { id: 6, subjects: [] },
+      ],
+    },
+    {
+      id: 4,
+      children: [],
+      competencies: [
+        { id: 6, subjects: [] },
+        { id: 3, subjects: [] },
+      ],
+    },
+    {
+      id: 3,
+      children: [],
+      competencies: [],
+    },
+  ];
+
+  expect(filterSocleBy2Ranks(state, socle, 23, 19)).toStrictEqual([
+    {
+      id: 4,
+      children: [],
+      competencies: [{ id: 3, subjects: [] }],
+    },
+  ]);
+});
+
+test("filterSocleBy3Ranks", () => {
+  const state = {
+    socle: {
+      containers: { 3: { rank: 3 }, 25: { rank: 19 }, 4: { rank: 23 } },
+      competencies: { 5: { rank: 19 }, 6: { rank: 23 }, 3: { rank: 19 } },
+    },
+  };
+  const socle = [
+    {
+      id: 4,
+      children: [
+        {
+          id: 3,
+          competencies: [{ id: 6, subjects: [] }],
+        },
+        {
+          id: 25,
+          competencies: [
+            { id: 5, subjects: [] },
+            { id: 6, subjects: [] },
+          ],
+        },
+      ],
+      competencies: [],
+    },
+  ];
+
+  expect(filterSocleBy3Ranks(state, socle, 23, 19, 23)).toStrictEqual([
+    {
+      id: 4,
+      children: [
+        {
+          id: 25,
+          competencies: [{ id: 6, subjects: [] }],
+        },
+      ],
+      competencies: [],
+    },
+  ]);
+});
+
+test("filterSocleByRegex", () => {
+  const state = {
+    socle: {
+      competencies: {
+        5: { text: "euh" },
+        6: { text: "MeuH" },
+        7: { text: " jdsqk" },
+        8: { text: "ah oui MEuH bonjour" },
+        9: { text: "ah oui M.EuH bonjour" },
+      },
+    },
+  };
+  const socle = [
+    {
+      id: 4,
+      children: [
+        {
+          id: 3,
+          competencies: [{ id: 7, subjects: [] }],
+        },
+        {
+          id: 25,
+          competencies: [
+            { id: 5, subjects: [] },
+            { id: 6, subjects: [] },
+          ],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 3,
+      children: [],
+      competencies: [
+        { id: 8, subjects: [] },
+        { id: 9, subjects: [] },
+      ],
+    },
+  ];
+
+  let regex = new RegExp("meuh", "i");
+  expect(filterSocleByRegex(state, socle, regex)).toStrictEqual([
+    {
+      id: 4,
+      children: [
+        {
+          id: 25,
+          competencies: [{ id: 6, subjects: [] }],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 3,
+      children: [],
+      competencies: [{ id: 8, subjects: [] }],
+    },
+  ]);
+});
+
+test("filterSocleByContainerId", () => {
+  const socle = [
+    {
+      id: 3,
+      children: [],
+      competencies: [],
+    },
+    {
+      id: 4,
+      children: [
+        {
+          id: 3,
+          competencies: [],
+        },
+        {
+          id: 25,
+          competencies: [],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 5,
+      children: [],
+      competencies: [],
+    },
+  ];
+  expect(filterSocleByContainerId(socle, 4)).toStrictEqual([
+    {
+      id: 4,
+      children: [
+        {
+          id: 3,
+          competencies: [],
+        },
+        {
+          id: 25,
+          competencies: [],
+        },
+      ],
+      competencies: [],
+    },
+  ]);
+});
+
+test("filterSocleByContainerIds", () => {
+  const socle = [
+    {
+      id: 3,
+      children: [],
+      competencies: [],
+    },
+    {
+      id: 4,
+      children: [
+        {
+          id: 3,
+          competencies: [],
+        },
+        {
+          id: 25,
+          competencies: [],
+        },
+        {
+          id: 26,
+          competencies: [],
+        },
+      ],
+      competencies: [],
+    },
+    {
+      id: 5,
+      children: [],
+      competencies: [],
+    },
+  ];
+  expect(filterSocleByContainerIds(socle, 4, 25)).toStrictEqual([
+    {
+      id: 4,
+      children: [
+        {
+          id: 25,
+          competencies: [],
+        },
+      ],
+      competencies: [],
+    },
+  ]);
 });
