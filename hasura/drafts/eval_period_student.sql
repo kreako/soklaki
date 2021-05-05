dc0646a511d16e90836b6cac42ec80620c809fa0
olivier <olivier@kreako.fr>
2021-05-05 12:25:47 +0200

    Filter eval_period_student by group_id

---
+++ hasura/drafts/eval_period_student.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW "public"."eval_period_student" AS
+SELECT eval_period.id as eval_period_id, student.id as student_id, estimate_cycle(eval_period.end, student.birthdate) as cycle
+  FROM eval_period
+  LEFT JOIN student ON (
+    (eval_period.group_id = student.group_id)
+    and
+    (
+      (student.school_exit is null and student.school_entry <= eval_period.end)
+      or
+      (student.school_exit is not null and
+       student.school_entry <= eval_period.end and
+       student.school_exit >= eval_period.start)
+    )
+  )
