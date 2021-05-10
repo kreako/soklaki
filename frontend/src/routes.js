978a48f062ab3756bcb3ffcdb2d98ec55827e2a0
olivier <olivier@kreako.fr>
2021-05-10 10:37:31 +0200

    Add a scrollbehavior to the router so that I go to the top of the page when a new link is opened

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -89,4 +89,11 @@ const routes = [
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
