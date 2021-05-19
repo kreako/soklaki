0e02b1d003abe14b85b1fa3000941ec0638ee835
olivier <olivier@kreako.fr>
2021-05-19 18:14:29 +0200

    Add sentry error surveillance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import { router } from './routes'
-import { store } from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import { router } from "./routes";
+import { store } from "./store";
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+import { version } from "../package.json";
 
-createApp(App)
-    .use(router)
-    .use(store)
-    .mount('#app')
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  release: "soklaki@" + version,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+const app = createApp(App).use(router).use(store);
+
+app.config.errorHandler = (err, vm, info) => {
+  Sentry.captureException(err);
+};
+
+app.mount("#app");
+
+window.addEventListener("error", (event) => {
+  Sentry.captureException(event);
+});
+window.addEventListener("unhandledrejection", (event) => {
+  Sentry.captureException(event);
+});
