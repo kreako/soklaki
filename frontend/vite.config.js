2d5564d6374d4bcb0941acd77fca2ea23030d2c1
olivier <olivier@kreako.fr>
2021-05-29 17:18:16 +0200

    dev server proxy for companion dl_report

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    proxy: {
+      "^/reports/.*": {
+        target: "http://127.0.0.1:8000/dl_report",
+      },
+    },
+  },
+});
