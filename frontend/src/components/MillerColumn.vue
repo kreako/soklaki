103e0c49bb557b285d10e952a3e20edaa29cf728
olivier <olivier@kreako.fr>
2021-06-14 15:19:52 +0200

    vue - Add skeleton of socle-edit

---
+++ frontend/src/components/MillerColumn.vue
@@ -0,0 +1,1 @@
+<template>MillerColumn</template>
---
+++ frontend/src/pages/SocleEdit.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="my-4 px-2">
+    <div>
+      <IconHome class="h-4" />
+    </div>
+    <div>
+      <MillerColumn />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { useRoute, useRouter } from "vue-router";
+import { useTitle } from "@vueuse/core";
+import { computed, ref, onMounted, watch } from "vue";
+import IconHome from "../icons/IconHome.vue";
+import MillerColumn from "../components/MillerColumn.vue";
+
+useTitle("Socle modification - soklaki.fr");
+
+const store = useStore();
+const route = useRoute();
+
+const socle = computed(() => store.state.socle);
+
+// TODO store actions
+// insertSocleContainer
+// insertSocleCompetency
+// insertSocleSubject
+// insertSocleCompetencySubject
+// updateSocleContainerActive
+// updateSocleCompetencyActive
+// updateSocleSubjectActive
+// updateSocleCompetencySubjectActive
+// updateSocleContainerContainerId
+// updateSocleCompetencyContainerId
+// updateSocleContainerRank
+// updateSocleCompetencyRank
+// updateSocleContainerText
+// updateSocleCompetencyText
+// updateSocleSubjectText
+</script>
