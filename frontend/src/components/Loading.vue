f2f031e63dd21b3379c0876f27a82ab32634a1b8
olivier <olivier@kreako.fr>
2021-06-20 17:38:28 +0200

    vue - Add a very basic loading component

---
+++ frontend/src/components/Loading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="loading">Loading...</div>
+  <div v-else>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  loading: Boolean,
+});
+</script>
