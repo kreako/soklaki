ea0a96cfebf5fc57d335a54b8f682918ad788d4d
olivier <olivier@kreako.fr>
2021-06-20 18:53:06 +0200

    vue - colorfull and animated loading component

--- frontend/src/components/Loading.vue
+++ frontend/src/components/Loading.vue
@@ -1,5 +1,23 @@
 <template>
-  <div v-if="loading">Loading...</div>
+  <div v-if="loading" class="min-h-full flex items-center justify-center p-4">
+    <div class="thing">
+      <div class="thing">
+        <div class="thing">
+          <div class="thing">
+            <div class="thing">
+              <div class="thing">
+                <div class="thing">
+                  <div class="thing">
+                    <div class="thing"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div v-else>
     <slot></slot>
   </div>
@@ -10,3 +28,22 @@ const props = defineProps({
   loading: Boolean,
 });
 </script>
+
+<style scope>
+.thing {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  border: 10px solid transparent;
+  border-color: #0d9488 #eab308 #dc2626 #db2777;
+  border-radius: 25%;
+  animation: thing 5s cubic-bezier(0.35, -0.14, 0.79, 1.22) infinite;
+}
+
+@keyframes thing {
+  100% {
+    transform: rotateZ(1turn);
+  }
+}
+</style>
