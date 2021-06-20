4321fbd3816e49eb710ccfab2d70199070f78b54
olivier <olivier@kreako.fr>
2021-06-20 17:38:55 +0200

    vue - Add deep linking in socle edit

--- frontend/src/pages/SocleEdit.vue
+++ frontend/src/pages/SocleEdit.vue
@@ -1,112 +1,118 @@
 <template>
   <div class="my-4 px-2">
-    <div>
-      <div
-        class="cursor-pointer hover:text-teal-500"
-        @click="selectCycle(null)"
-      >
-        <IconHome class="h-4" />
-      </div>
-      <div v-if="selectedCycle" class="pl-2">
-        <div class="cursor-pointer hover:text-teal-500" @click="selectL1(null)">
-          {{ cycle.text }}
+    <Loading :loading="loading">
+      <div>
+        <div
+          class="cursor-pointer hover:text-teal-500"
+          @click="selectCycle(null)"
+        >
+          <IconHome class="h-4" />
         </div>
-        <div v-if="selectedL1" class="pl-2">
+        <div v-if="selectedCycle" class="pl-2">
           <div
             class="cursor-pointer hover:text-teal-500"
-            @click="selectL2(null)"
+            @click="selectL1(null)"
           >
-            {{ containerById(containerL1.id).full_rank }}
-            {{ containerById(containerL1.id).text }}
+            {{ cycle.text }}
           </div>
-          <div v-if="selectedL2" class="pl-2">
+          <div v-if="selectedL1" class="pl-2">
             <div
               class="cursor-pointer hover:text-teal-500"
-              @click="selectCompetency(null)"
+              @click="selectL2(null)"
             >
-              {{ containerById(containerL2.id).full_rank }}
-              {{ containerById(containerL2.id).text }}
+              {{ containerById(containerL1.id).full_rank }}
+              {{ containerById(containerL1.id).text }}
+            </div>
+            <div v-if="selectedL2" class="pl-2">
+              <div
+                class="cursor-pointer hover:text-teal-500"
+                @click="selectCompetency(null)"
+              >
+                {{ containerById(containerL2.id).full_rank }}
+                {{ containerById(containerL2.id).text }}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="flex flex-row items-stretch mt-8">
-      <div class="flex-grow">
-        <MillerColumn
-          :list="cycles"
-          title="Cycles"
-          @selected="selectCycle"
-          :hide="selectedCycle != null"
-          :editable="false"
-          v-slot="{ item }"
-        >
-          {{ item.text }}
-        </MillerColumn>
-        <MillerColumn
-          v-if="selectedCycle"
-          :list="socle[selectedCycle]"
-          title="Domaines"
-          @selected="selectL1"
-          :hide="selectedL1 != null"
-          v-slot="{ item }"
-        >
-          {{ containerById(item.id).full_rank }}
-          {{ containerById(item.id).text }}
-        </MillerColumn>
-        <div v-if="selectedL1">
-          <div v-if="containerL1.children.length > 0">
-            <MillerColumn
-              :list="containerL1.children"
-              title="Sous domaines"
-              @selected="selectL2"
-              :hide="selectedL2 != null"
-              v-slot="{ item }"
-            >
-              {{ containerById(item.id).full_rank }}
-              {{ containerById(item.id).text }}
-            </MillerColumn>
-            <MillerColumn
-              v-if="selectedL2"
-              :list="containerL2.competencies"
-              title="Compétences"
-              @selected="selectCompetency"
-              :hide="selectedCompetency != null"
-              v-slot="{ item }"
-            >
-              {{ competencyById(item.id).full_rank }}
-              {{ competencyById(item.id).text }}
-            </MillerColumn>
-            <div v-if="selectedCompetency">
-              <SocleEditCompetency :competency="competency" />
+      <div class="flex flex-row items-stretch mt-8">
+        <div class="flex-grow">
+          <MillerColumn
+            :list="cycles"
+            title="Cycles"
+            @selected="selectCycle"
+            :hide="selectedCycle != null"
+            :editable="false"
+            v-slot="{ item }"
+          >
+            {{ item.text }}
+          </MillerColumn>
+          <MillerColumn
+            v-if="selectedCycle"
+            :list="socle[selectedCycle]"
+            title="Domaines"
+            @selected="selectL1"
+            :hide="selectedL1 != null"
+            v-slot="{ item }"
+          >
+            {{ containerById(item.id).full_rank }}
+            {{ containerById(item.id).text }}
+          </MillerColumn>
+          <div v-if="selectedL1">
+            <div v-if="containerL1.children.length > 0">
+              <MillerColumn
+                :list="containerL1.children"
+                title="Sous domaines"
+                @selected="selectL2"
+                :hide="selectedL2 != null"
+                v-slot="{ item }"
+              >
+                {{ containerById(item.id).full_rank }}
+                {{ containerById(item.id).text }}
+              </MillerColumn>
+              <MillerColumn
+                v-if="selectedL2"
+                :list="containerL2.competencies"
+                title="Compétences"
+                @selected="selectCompetency"
+                :hide="selectedCompetency != null"
+                v-slot="{ item }"
+              >
+                {{ competencyById(item.id).full_rank }}
+                {{ competencyById(item.id).text }}
+              </MillerColumn>
+              <div v-if="selectedCompetency">
+                <SocleEditCompetency :competency="competency" />
+              </div>
             </div>
-          </div>
-          <div v-else>
-            <MillerColumn
-              :list="containerL1.competencies"
-              title="Compétences"
-              @selected="selectCompetency"
-              :hide="selectedCompetency != null"
-              v-slot="{ item }"
-            >
-              {{ competencyById(item.id).full_rank }}
-              {{ competencyById(item.id).text }}
-            </MillerColumn>
-            <div v-if="selectedCompetency">
-              <SocleEditCompetency :competency="competency" />
+            <div v-else>
+              <MillerColumn
+                :list="containerL1.competencies"
+                title="Compétences"
+                @selected="selectCompetency"
+                :hide="selectedCompetency != null"
+                v-slot="{ item }"
+              >
+                {{ competencyById(item.id).full_rank }}
+                {{ competencyById(item.id).text }}
+              </MillerColumn>
+              <div v-if="selectedCompetency">
+                <SocleEditCompetency :competency="competency" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Loading>
   </div>
 </template>
 
 <script setup>
 import { useStore } from "vuex";
 import { useRoute, useRouter } from "vue-router";
-import { useTitle } from "@vueuse/core";
-import { computed, ref, onMounted, watch } from "vue";
+import { useTitle, until } from "@vueuse/core";
+import { computed, ref, onMounted, watchEffect } from "vue";
+import Loading from "../components/Loading.vue";
 import IconHome from "../icons/IconHome.vue";
 import IconChevronLeft from "../icons/IconChevronLeft.vue";
 import IconChevronRight from "../icons/IconChevronRight.vue";
@@ -117,11 +123,14 @@ useTitle("Socle modification - soklaki.fr");
 
 const store = useStore();
 const route = useRoute();
+const router = useRouter();
 
 const socle = computed(() => store.state.socle);
 
 const containerById = computed(() => store.getters.containerById);
 const competencyById = computed(() => store.getters.competencyById);
+
+const loading = ref(true);
 
 const cycles = [
   { id: "c1", text: "Cycle 1" },
@@ -130,7 +139,7 @@ const cycles = [
   { id: "c4", text: "Cycle 4" },
 ];
 
-const selectedCycle = ref(null);
+const selectedCycle = ref(route.query.cycle);
 const selectCycle = async (id) => {
   selectedCycle.value = id;
   selectedL1.value = null;
@@ -145,7 +154,7 @@ const cycle = computed(() => {
   return cycles.find((x) => x.id === selectedCycle.value);
 });
 
-const selectedL1 = ref(null);
+const selectedL1 = ref(route.query.l1);
 const selectL1 = async (id) => {
   selectedL1.value = id;
   selectedL2.value = null;
@@ -157,11 +166,11 @@ const containerL1 = computed(() => {
     return store.getters.containerById(null);
   }
   return store.state.socle[selectedCycle.value].find(
-    (x) => x.id === selectedL1.value
+    (x) => x.id == selectedL1.value
   );
 });
 
-const selectedL2 = ref(null);
+const selectedL2 = ref(route.query.l2);
 const selectL2 = async (id) => {
   selectedL2.value = id;
   selectedCompetency.value = null;
@@ -175,48 +184,47 @@ const containerL2 = computed(() => {
     return store.getters.containerById(null);
   }
   return store.state.socle[selectedCycle.value]
-    .find((x) => x.id === selectedL1.value)
-    .children.find((x) => x.id === selectedL2.value);
+    .find((x) => x.id == selectedL1.value)
+    .children.find((x) => x.id == selectedL2.value);
 });
 
-const selectedCompetency = ref(null);
+const selectedCompetency = ref(route.query.competency);
 const selectCompetency = async (id) => {
   selectedCompetency.value = id;
 };
 
 const competency = computed(() => {
   if (selectedCycle.value == null) {
-    return store.getters.containerById(null);
+    return store.getters.competencyById(null);
   }
   if (selectedL1.value == null) {
-    return store.getters.containerById(null);
+    return store.getters.competencyById(null);
   }
   if (selectedL2.value == null) {
     return store.state.socle[selectedCycle.value]
-      .find((x) => x.id === selectedL1.value)
-      .competencies.find((x) => x.id === selectedCompetency.value);
+      .find((x) => x.id == selectedL1.value)
+      .competencies.find((x) => x.id == selectedCompetency.value);
   } else {
     return store.state.socle[selectedCycle.value]
-      .find((x) => x.id === selectedL1.value)
-      .children.find((x) => x.id === selectedL2.value)
-      .competencies.find((x) => x.id === selectedCompetency.value);
+      .find((x) => x.id == selectedL1.value)
+      .children.find((x) => x.id == selectedL2.value)
+      .competencies.find((x) => x.id == selectedCompetency.value);
   }
 });
 
-// TODO store actions
-// insertSocleContainer
-// insertSocleCompetency
-// insertSocleSubject
-// insertSocleCompetencySubject
-// updateSocleContainerActive
-// updateSocleCompetencyActive
-// updateSocleSubjectActive
-// updateSocleCompetencySubjectActive
-// updateSocleContainerContainerId
-// updateSocleCompetencyContainerId
-// updateSocleContainerRank
-// updateSocleCompetencyRank
-// updateSocleContainerText
-// updateSocleCompetencyText
-// updateSocleSubjectText
+watchEffect(() => {
+  router.replace({
+    query: {
+      cycle: selectedCycle.value,
+      l1: selectedL1.value,
+      l2: selectedL2.value,
+      competency: selectedCompetency.value,
+    },
+  });
+});
+
+onMounted(async () => {
+  await until(() => store.state.socle.c1.length > 0).toBeTruthy();
+  loading.value = false;
+});
 </script>
