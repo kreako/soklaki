87ca4d2a8c6ecb4a5914919ceaa1bb313c420d37
olivier <olivier@kreako.fr>
2021-06-28 14:17:26 +0200

    vue - Display observations during evaluation by competency

--- frontend/src/pages/EvaluationCompetency.vue
+++ frontend/src/pages/EvaluationCompetency.vue
@@ -25,6 +25,44 @@
             {{ student.lastname }}
           </div>
           <div class="mt-2">
+            <div
+              v-if="evaluationByStudent[student.id].observations.length > 0"
+              class="text-sm"
+            >
+              <Disclosure>
+                <DisclosureButton>
+                  <div
+                    class="text-gray-700 flex flex-row items-center space-x-2"
+                  >
+                    <div>
+                      {{ evaluationByStudent[student.id].observations.length }}
+                      observations
+                    </div>
+                    <IconChevronDown class="h-4 text-gray-400" />
+                  </div>
+                </DisclosureButton>
+                <DisclosurePanel>
+                  <div
+                    v-for="o in evaluationByStudent[student.id].observations"
+                    class="mb-1"
+                  >
+                    <div class="flex flex-row space-x-4 items-center">
+                      <div>
+                        {{ o.date }}
+                      </div>
+                      <div
+                        class="text-xs rounded-full px-1 border border-gray-600"
+                      >
+                        {{ userInitials(userById(o.user_id)) }}
+                      </div>
+                    </div>
+                    <div>
+                      {{ o.text }}
+                    </div>
+                  </div>
+                </DisclosurePanel>
+              </Disclosure>
+            </div>
             <EvalCompetency
               :edit="true"
               :comment="evaluationByStudent[student.id].comment"
@@ -81,12 +119,15 @@ import { useRoute, useRouter } from "vue-router";
 import { until } from "@vueuse/core";
 import { today } from "../utils/date";
 import { fathers } from "../utils/competency";
+import { userInitials } from "../utils/user";
 import Loading from "../components/Loading.vue";
 import EvalCompetency from "../components/EvalCompetency.vue";
 import IconChevronLeft from "../icons/IconChevronLeft.vue";
 import IconChevronRight from "../icons/IconChevronRight.vue";
 import IconChevronUp from "../icons/IconChevronUp.vue";
+import IconChevronDown from "../icons/IconChevronDown.vue";
 import { useTitle } from "@vueuse/core";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 
 useTitle("Évaluation d'une compétence - soklaki.fr");
 
@@ -99,9 +140,8 @@ const competencyId = computed(() =>
   route.params.id == null ? null : Number(route.params.id)
 );
 const competencyById = computed(() => store.getters.competencyById);
-
 const studentById = computed(() => store.getters.studentById);
-
+const userById = computed(() => store.getters.userById);
 const containerById = computed(() => store.getters.containerById);
 const competencyFathers = computed(() => fathers(store, competencyId.value));
 
@@ -134,18 +174,26 @@ const evaluationByStudent = computed(() => {
   for (const student of students.value) {
     // Search for an existing evaluation
     const evaluation = searchEvaluation(student.id);
+    // Search for existing observations
+    const obs = observations.value.filter(
+      (o) =>
+        o.competencies.find((c) => c.competency_id == route.params.id) !=
+          null && o.students.find((c) => c.student_id == student.id) != null
+    );
     if (evaluation == null) {
       // No evaluation yet
       e[student.id] = {
         id: null,
         comment: null,
         status: "Emtpy",
+        observations: obs,
       };
     } else {
       e[student.id] = {
         id: evaluation.id,
         comment: evaluation.comment,
         status: evaluation.status,
+        observations: obs,
       };
     }
   }
