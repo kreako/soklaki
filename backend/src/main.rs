efb816fcc704f5a754f048950e7f1a85567fb8c9
olivier <olivier@kreako.fr>
2021-07-14 19:43:05 +0200

    backend - Add a first rocket handler connecting to db

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket_sync_db_pools::database;
+use rocket_sync_db_pools::postgres;
+
+#[database("postgres")]
+struct Db(postgres::Client);
+
+#[get("/")]
+async fn index(db: Db) -> &'static str {
+    db.run(|client| {
+        for row in client
+            .query(
+                "SELECT id, firstname FROM public.user WHERE firstname is not null",
+                &[],
+            )
+            .unwrap()
+        {
+            let id: i64 = row.get(0);
+            let name: &str = row.get(1);
+            println!("{} : {}", id, name);
+        }
+    })
+    .await;
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Db::fairing())
+        .mount("/", routes![index])
 }
