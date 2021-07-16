55f8792d5d875b40c2242890ab1a9b30671891a3
olivier <olivier@kreako.fr>
2021-07-17 00:44:00 +0200

    backend - cargo fmt

--- backend/src/auth_header.rs
+++ backend/src/auth_header.rs
@@ -12,35 +12,35 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AuthHeader {
-	pub raw: String,
+    pub raw: String,
 }
 
 #[derive(Debug, Serialize)]
 pub struct AuthHeaderResponse {
-	pub absent_auth_header: bool,
+    pub absent_auth_header: bool,
 }
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for AuthHeader {
-	type Error = status::Custom<Json<AuthHeaderResponse>>;
-	async fn from_request(
-		request: &'r Request<'_>,
-	) -> request::Outcome<Self, status::Custom<Json<AuthHeaderResponse>>> {
-		if let Some(authen_header) = request.headers().get_one("Authorization") {
-			let authen_str = authen_header.to_string();
-			if authen_str.starts_with("Bearer") {
-				return Outcome::Success(AuthHeader { raw: authen_str });
-			}
-		}
+    type Error = status::Custom<Json<AuthHeaderResponse>>;
+    async fn from_request(
+        request: &'r Request<'_>,
+    ) -> request::Outcome<Self, status::Custom<Json<AuthHeaderResponse>>> {
+        if let Some(authen_header) = request.headers().get_one("Authorization") {
+            let authen_str = authen_header.to_string();
+            if authen_str.starts_with("Bearer") {
+                return Outcome::Success(AuthHeader { raw: authen_str });
+            }
+        }
 
-		Outcome::Failure((
-			Status::BadRequest,
-			status::Custom(
-				Status::Unauthorized,
-				Json(AuthHeaderResponse {
-					absent_auth_header: true,
-				}),
-			),
-		))
-	}
+        Outcome::Failure((
+            Status::BadRequest,
+            status::Custom(
+                Status::Unauthorized,
+                Json(AuthHeaderResponse {
+                    absent_auth_header: true,
+                }),
+            ),
+        ))
+    }
 }
