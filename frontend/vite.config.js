1090818bb2796241afd5df0a63f8e084ffab3452
olivier <olivier@kreako.fr>
2021-07-17 19:42:45 +0200

    frontend - vite config to correctly rewrite request path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       "^/api/.*": {
         target: "http://127.0.0.1:8008/",
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "^/reports/.*": {
         target: "http://127.0.0.1:8000/dl_report",
