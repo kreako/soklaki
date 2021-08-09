20a4a57dadda18f60aedd348f5f2bc110f5c6ac1
Kreako <olivier@kreako.fr>
2021-08-09 14:12:18 +0200

    hasura - Add index on socle_competency cycle

---
+++ hasura/migrations/default/1628511096806_socle_competency_cycle_idx/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE INDEX socle_competency_cycle_idx ON socle_competency(cycle);
---
+++ hasura/migrations/default/1628511096806_socle_competency_cycle_idx/up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX socle_competency_cycle_idx ON socle_competency(cycle);
