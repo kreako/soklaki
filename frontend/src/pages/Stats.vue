c5830f59cca5c6848c5cdaf0bd0e39a2aba5b9a4
Kreako <olivier@kreako.fr>
2021-08-09 16:47:57 +0200

    frontend - Refactor Stats page to use the new endpoint

--- frontend/src/pages/Stats.vue
+++ frontend/src/pages/Stats.vue
@@ -1,157 +1,214 @@
 <template>
   <div class="my-4 px-2">
-    <div class="form-label">
-      Plein de statistiques sur le cycle {{ cycleNb(route.params.cycle) }}
-    </div>
-    <div class="mt-12">
-      <div v-for="slice in students">
-        <div class="flex flex-row mt-20">
-          <div class="w-16"></div>
-          <div
-            v-for="studentId in slice"
-            class="text-xs w-8 self-end writing-mode-vertical-lr"
-          >
-            {{ studentById(studentId).firstname }}
-            {{ studentById(studentId).lastname }}
-          </div>
-        </div>
-        <div class="mt-2">
-          <div
-            v-for="(statByStudents, competencyId) in stats.stats"
-            class="flex flex-row items-center leading-tight"
-          >
-            <div class="w-16 text-gray-700 text-right pr-2 hover:text-teal-500">
-              <button @click="selectCompetency(competencyId)">
-                {{ competencyById(competencyId).full_rank }}
-              </button>
-            </div>
-            <div v-for="studentId in slice" class="flex flex-row space-x-1 w-8">
-              <button
-                @click="selectObservation(competencyId, studentId)"
-                :class="
-                  statByStudents[studentId].observations > 0
-                    ? 'bg-green-500'
-                    : 'bg-red-500'
-                "
-                class="w-2 h-2"
-              ></button>
-              <router-link
-                :to="`/evaluation-single/${route.params.cycle}/${competencyId}/${studentId}`"
-                :class="{
-                  'bg-red-500':
-                    statByStudents[studentId].evaluations.status === 'Empty',
-                  'bg-blue-500':
-                    statByStudents[studentId].evaluations.status ===
-                    'NotAcquired',
-                  'bg-teal-500':
-                    statByStudents[studentId].evaluations.status ===
-                    'InProgress',
-                  'bg-green-500':
-                    statByStudents[studentId].evaluations.status ===
-                      'Acquired' ||
-                    statByStudents[studentId].evaluations.status === 'TipTop',
-                }"
-                class="w-2 h-2"
-              ></router-link>
-            </div>
-          </div>
-        </div>
+    <Loading :loading="loading">
+      <div class="form-label">
+        Plein de statistiques sur le cycle {{ cycleNb(route.params.cycle) }}
       </div>
-    </div>
-    <Modal
-      :title="competencyModalTitle"
-      :show="showCompetencyModal"
-      @close="closeCompetencyModal"
-    >
-      <div>
-        <div class="uppercase tracking-wide text-gray-700">
-          {{ competencyFathers[0].rank }}.
-          {{ competencyFathers[0].text }}
-        </div>
-        <div v-if="competencyFathers[1].rank != null" class="text-gray-700">
-          {{ competencyFathers[1].rank }}.
-          {{ competencyFathers[1].text }}
-        </div>
-        <div>
-          {{ competencyById(selectedCompetency).rank }}.
-          {{ competencyById(selectedCompetency).text }}
-        </div>
-        <div class="mt-2 pl-2">
-          <HashSubjects
-            :subjects="competencyById(selectedCompetency).subjects"
-          />
-        </div>
-        <div class="mt-2 pl-2">
-          <CompetencyTemplates
-            :templates="competencyById(selectedCompetency).templates"
-          />
-        </div>
-      </div>
-    </Modal>
-    <Modal
-      :title="observationModalTitle"
-      :show="showObservationModal"
-      @close="closeObservationModal"
-    >
-      <div>
-        <div class="uppercase tracking-wide text-gray-700 text-xs">
-          {{ competencyFathers[0].rank }}.
-          {{ competencyFathers[0].text }}
-        </div>
-        <div
-          v-if="competencyFathers[1].rank != null"
-          class="text-gray-700 text-xs"
-        >
-          {{ competencyFathers[1].rank }}.
-          {{ competencyFathers[1].text }}
-        </div>
-        <div class="text-sm">
-          {{ competencyById(selectedCompetency).rank }}.
-          {{ competencyById(selectedCompetency).text }}
-        </div>
-        <div class="pl-2">
-          <HashSubjects
-            :subjects="competencyById(selectedCompetency).subjects"
-          />
-        </div>
-        <div
-          v-if="competencyById(selectedCompetency).templates.length > 0"
-          class="mt-8 pl-2"
-        >
-          <div class="font-bold">
-            Créer une nouvelle observation à partir d'un modèle
-          </div>
-          <ul class="mt-2">
-            <li
-              v-for="template in competencyById(selectedCompetency).templates"
-              class="mt-1 hover:text-teal-500"
-            >
-              <router-link
-                :to="`/new-observation-from-template/${selectedStudent}/${selectedCompetency}/${template.id}`"
+      <div
+        class="
+          my-4
+          overflow-auto
+          w-full
+          h-[calc(100vh-8rem)]
+          md:h-[calc(100vh-4rem)]
+          max-w-[calc(100vw-2*1rem)]
+        "
+      >
+        <table class="relative table-fixed border-collapse w-full">
+          <thead>
+            <tr>
+              <th class="w-16 sticky top-0 left-0 z-[3] bg-white">&nbsp;</th>
+              <th
+                v-for="(student, idx) in stats2.students"
+                :key="student.id"
+                colspan="2"
+                class="font-bold w-16 sticky top-0 z-[2] text-center"
+                :class="idx % 2 === 1 ? 'bg-gray-200' : 'bg-white'"
               >
-                {{ templateById(template.id).text }}
-              </router-link>
-            </li>
-            <li class="mt-3 hover:text-teal-500">
+                <div
+                  class="
+                    w-8
+                    flex-shrink-0 flex flex-row
+                    justify-center
+                    py-2
+                    mx-auto
+                  "
+                >
+                  <div class="writing-mode-vertical-lr font-normal">
+                    {{ student.firstname }}
+                    {{ student.lastname }}
+                  </div>
+                </div>
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="byCompetency in stats2.stats"
+              :key="byCompetency.competency.id"
+            >
+              <td class="sticky left-0 h-6 z-[1] bg-white text-center">
+                <button @click="selectCompetency(byCompetency.competency.id)">
+                  {{ byCompetency.competency.full_rank }}
+                </button>
+              </td>
+              <template
+                v-for="(byStudent, idx) in byCompetency.by_students"
+                :key="byStudent.id"
+              >
+                <td
+                  class="w-8 h-6 text-center"
+                  :class="idx % 2 === 1 ? 'bg-gray-200' : 'bg-white'"
+                >
+                  <div class="block ml-2">
+                    <button
+                      @click="
+                        selectObservation(
+                          byCompetency.competency.id,
+                          byStudent.student_id
+                        )
+                      "
+                      :class="
+                        byStudent.observations > 0
+                          ? 'bg-green-500'
+                          : 'bg-gray-500'
+                      "
+                      class="
+                        align-middle
+                        w-5
+                        h-5
+                        text-xs
+                        rounded-md
+                        block
+                        mx-auto
+                      "
+                    >
+                      {{ byStudent.observations }}
+                    </button>
+                  </div>
+                </td>
+                <td
+                  class="w-8 h-6 text-center"
+                  :class="idx % 2 === 1 ? 'bg-gray-200' : 'bg-white'"
+                >
+                  <div class="block mr-2">
+                    <router-link
+                      :to="`/evaluation-single/${route.params.cycle}/${byCompetency.competency.id}/${byStudent.student_id}`"
+                      :class="{
+                        'bg-gray-500': byStudent.evaluation === 'Empty',
+                        'bg-red-500': byStudent.evaluation === 'NotAcquired',
+                        'bg-yellow-500': byStudent.evaluation === 'InProgress',
+                        'bg-green-500': byStudent.evaluation === 'Acquired',
+                        'bg-pink-500': byStudent.evaluation === 'TipTop',
+                      }"
+                      class="w-5 h-5 rounded-md align-middle block mx-auto"
+                    >
+                    </router-link>
+                  </div>
+                </td>
+              </template>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <Modal
+        :title="competencyModalTitle"
+        :show="showCompetencyModal"
+        @close="closeCompetencyModal"
+      >
+        <div>
+          <div class="uppercase tracking-wide text-gray-700">
+            {{ competencyFathers[0].rank }}.
+            {{ competencyFathers[0].text }}
+          </div>
+          <div v-if="competencyFathers[1].rank != null" class="text-gray-700">
+            {{ competencyFathers[1].rank }}.
+            {{ competencyFathers[1].text }}
+          </div>
+          <div>
+            {{ competencyById(selectedCompetency).rank }}.
+            {{ competencyById(selectedCompetency).text }}
+          </div>
+          <div class="mt-2 pl-2">
+            <HashSubjects
+              :subjects="competencyById(selectedCompetency).subjects"
+            />
+          </div>
+          <div class="mt-2 pl-2">
+            <CompetencyTemplates
+              :templates="competencyById(selectedCompetency).templates"
+            />
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        :title="observationModalTitle"
+        :show="showObservationModal"
+        @close="closeObservationModal"
+      >
+        <div>
+          <div class="uppercase tracking-wide text-gray-700 text-xs">
+            {{ competencyFathers[0].rank }}.
+            {{ competencyFathers[0].text }}
+          </div>
+          <div
+            v-if="competencyFathers[1].rank != null"
+            class="text-gray-700 text-xs"
+          >
+            {{ competencyFathers[1].rank }}.
+            {{ competencyFathers[1].text }}
+          </div>
+          <div class="text-sm">
+            {{ competencyById(selectedCompetency).rank }}.
+            {{ competencyById(selectedCompetency).text }}
+          </div>
+          <div class="pl-2">
+            <HashSubjects
+              :subjects="competencyById(selectedCompetency).subjects"
+            />
+          </div>
+          <div
+            v-if="competencyById(selectedCompetency).templates.length > 0"
+            class="mt-8 pl-2"
+          >
+            <div class="font-bold">
+              Créer une nouvelle observation à partir d'un modèle
+            </div>
+            <ul class="mt-2">
+              <li
+                v-for="template in competencyById(selectedCompetency).templates"
+                class="mt-1 hover:text-teal-500"
+              >
+                <router-link
+                  :to="`/new-observation-from-template/${selectedStudent}/${selectedCompetency}/${template.id}`"
+                >
+                  {{ templateById(template.id).text }}
+                </router-link>
+              </li>
+              <li class="mt-3 hover:text-teal-500">
+                <router-link
+                  :to="`/new-observation-from-template/${selectedStudent}/${selectedCompetency}/${-1}`"
+                >
+                  Sans modèle
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <div v-else class="mt-8 pl-2">
+            <div class="hover:text-teal-500">
               <router-link
                 :to="`/new-observation-from-template/${selectedStudent}/${selectedCompetency}/${-1}`"
               >
-                Sans modèle
+                Créer une nouvelle observation
               </router-link>
-            </li>
-          </ul>
-        </div>
-        <div v-else class="mt-8 pl-2">
-          <div class="hover:text-teal-500">
-            <router-link
-              :to="`/new-observation-from-template/${selectedStudent}/${selectedCompetency}/${-1}`"
-            >
-              Créer une nouvelle observation
-            </router-link>
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </Loading>
   </div>
 </template>
 
@@ -163,6 +220,7 @@ import { until, useTitle } from "@vueuse/core";
 import { cycleNb } from "../utils/cycle";
 import { fathers } from "../utils/competency";
 import Modal from "../components/Modal.vue";
+import Loading from "../components/Loading.vue";
 import { useBreakpoints, breakpointsTailwind } from "@vueuse/core";
 import HashSubjects from "../components/HashSubjects.vue";
 import CompetencyTemplates from "../components/CompetencyTemplates.vue";
@@ -222,34 +280,12 @@ const competencyFathers = computed(() =>
   fathers(store, selectedCompetency.value)
 );
 
-const students = computed(() => {
-  if (store.state.stats[route.params.cycle].studentsCount === null) {
-    return [];
-  }
-  const full = store.state.periods[store.state.currentPeriod].students.map(
-    (x) => x.student.id
-  );
-  const filtered = full.filter(
-    (x) =>
-      store.state.students[x].current_cycle.current_cycle === route.params.cycle
-  );
-  let sliceSize = 20;
-  if (mobile.value) {
-    sliceSize = 7;
-  }
-  const students = [];
-  let index = 0;
-  while (true) {
-    const slice = filtered.slice(index, index + sliceSize);
-    if (slice.length === 0) {
-      return students;
-    }
-    students.push(slice);
-    index = index + sliceSize;
-  }
-});
+const loading = ref(true);
+const stats2 = ref({});
 
 onMounted(async () => {
+  stats2.value = await store.dispatch("statsByCycle", route.params.cycle);
+  loading.value = false;
   await until(() => store.state.currentPeriod).not.toBeNull();
   await store.dispatch("stats", { periodId: store.state.currentPeriod });
 });
@@ -258,5 +294,8 @@ onMounted(async () => {
 <style>
 .writing-mode-vertical-lr {
   writing-mode: vertical-lr;
+}
+.full-width {
+  width: calc(100vw - 3 * 0.5rem);
 }
 </style>
