e0b8c25ea2449f3c1ff2dc61e9ad8cbb8dd84a72
kreako <olivier@kreako.fr>
2022-03-16 10:48:33 +0100

    hasura : summary_report_period view definition

---
+++ hasura/migrations/default/1647509449375_summary_report_period/down.sql
@@ -0,0 +1,21 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE
+-- OR REPLACE VIEW "public"."summary_report_period" AS
+-- SELECT
+--   eval_period.id AS eval_period_id,
+--   summary_report.id AS summary_report_id
+-- FROM
+--   (
+--     (
+--       eval_period
+--       JOIN student ON ((eval_period.group_id = student.group_id))
+--     )
+--     JOIN summary_report ON (
+--       (
+--         (summary_report.student_id = student.id)
+--         AND (summary_report.date >= eval_period.start)
+--         AND (summary_report.date <= eval_period."end")
+--       )
+--     )
+--   );
